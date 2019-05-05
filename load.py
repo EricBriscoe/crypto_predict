58b94e035caec0aa91284061ab75d0bee733b972
ericbriscoe <eric0briscoe@gmail.com>
2019-05-04 19:00:24 -0500

    Add training code
    (cherry picked from commit 06abb74dcbfb958e3463b938372e1c023447bcb8)
    
    # Conflicts:
    #	load.py
    #	train.py

--- load.py
+++ load.py
@@ -5,11 +5,11 @@ import numpy as np
 import pandas as pd
 import sqlalchemy
 from binance.client import Client
+from sklearn import preprocessing
 from tqdm import tqdm
 
 api_key = os.environ["api_key"]
 secret_key = os.environ["secret_key"]
-client = Client(api_key, secret_key)
 
 
 def grab_engine():
@@ -21,6 +21,7 @@ def grab_engine():
 def save_klines(kline_list):
     engine = grab_engine()
     formatted_klines = []
+    client = Client(api_key, secret_key)
     for kline in kline_list:
         kline.pop(-1)
         formatted_klines.append(list(map(float, kline)))
@@ -43,11 +44,12 @@ def save_klines(kline_list):
     )
     df.set_index(keys="open_time", inplace=True)
     print(df)
-    df.to_sql(name="binance_klines", con=engine, if_exists="append")
-    return df
+    df.to_sql(name="binance_klines", con=engine, if_exists="replace")
 
 
-def load_training_data(train_tables, test_tables, predict_time, rows_per_table):
+def load_training_data(
+    train_tables, test_tables, predict_time, rows_per_table, predict_param
+):
     engine = grab_engine()
     rows = pd.read_sql(sql="SELECT COUNT(open_time) FROM binance_klines", con=engine)[
         "count"
@@ -60,45 +62,41 @@ def load_training_data(train_tables, test_tables, predict_time, rows_per_table):
     df = pd.read_sql(
         sql="SELECT open, high, low, close, volume FROM binance_klines", con=engine
     )
+    x = df.values  # returns a numpy array
+    min_max_scaler = preprocessing.MinMaxScaler()
+    x_scaled = min_max_scaler.fit_transform(x)
+    df = pd.DataFrame(x_scaled, columns=["open", "high", "low", "close", "volume"])
 
-    mults = {col: df[col].max() for col in df.columns}
-    for col in df.columns:
-        df[col] = df[col].apply(lambda x: x / mults[col])
-
-    random_offset = 0
-
-    x_train = []
-    x_test = []
+    x_train = np.ndarray(shape=(0, rows_per_table, 5))
+    x_test = np.ndarray(shape=(0, rows_per_table, 5))
 
     print("Loading X Training Data")
     t.sleep(0.1)
     for i in tqdm(range(train_tables)):
-        x_train.append(df[i : i + rows_per_table].values)
-    x_train = np.array(x_train)
+        x_train = np.append(x_train, df[i: i + rows_per_table].values)
     t.sleep(0.1)
     print("Loading Y Training Data")
     sub_df = df[
         rows_per_table + predict_time : rows_per_table + predict_time + train_tables
     ]
-    sub_df = sub_df.drop("volume", axis=1)
-    y_train = sub_df.values
+    y_train = sub_df[predict_param].values
     print("Loading X Testing Data")
     t.sleep(0.1)
     for i in tqdm(range(test_tables)):
-        x_test.append(df[i + train_tables : i + train_tables + rows_per_table].values)
-    x_test = np.array(x_test)
+        x_test = np.append(
+            x_test, df[i + train_tables: i + train_tables + rows_per_table].values
+        )
     t.sleep(0.1)
     print("Loading Y Testing Data")
     sub_df = df[
-        rows_per_table
-        + predict_time
-        + train_tables : rows_per_table
-        + predict_time
-        + test_tables
-        + train_tables
-    ]
-    sub_df = sub_df.drop("volume", axis=1)
-    y_test = sub_df.values
+             rows_per_table
+             + predict_time
+             + train_tables: rows_per_table
+                             + predict_time
+                             + test_tables
+                             + train_tables
+             ]
+    y_test = sub_df[predict_param].values
     print(f"X Training Data Structure: ({x_train.shape})")
     print(f"Y Training Data Structure: ({y_train.shape})")
     print(f"X Testing  Data Structure: ({x_test.shape})")
@@ -108,16 +106,16 @@ def load_training_data(train_tables, test_tables, predict_time, rows_per_table):
 
 
 if __name__ == "__main__":
-    days_ago = 0
-    while True:
-        days_ago += 1
-        klines = client.get_historical_klines(
-            "BNBBTC",
-            Client.KLINE_INTERVAL_1MINUTE,
-            f"{days_ago} day ago UTC",
-            f"{days_ago - 1} day ago UTC",
-        )
-        dframe = save_klines(klines)
-        if len(dframe) == 0:
-            break
-        print(f"Saved data for {days_ago} days ago")
+    # days_ago = 100
+    # klines = client.get_historical_klines(
+    #     "BNBBTC", Client.KLINE_INTERVAL_1MINUTE, f"{days_ago} day ago UTC"
+    #     )
+    # save_klines(klines)
+    # print(f"Saved data for {days_ago} days ago")
+    load_training_data(
+        train_tables=1000,
+        test_tables=200,
+        predict_time=1,
+        rows_per_table=100,
+        predict_param="high",
+    )
--- train.py
+++ train.py
@@ -2,45 +2,31 @@ import tensorflow as tf
 
 import load
 
-hours_of_data = 12
-rows = hours_of_data * 60
 
-
-def build_model():
-    model_frame = tf.keras.Sequential(
-        [
-            tf.keras.layers.Flatten(input_shape=(rows, 5)),
-            tf.keras.layers.Dense(500),
-            tf.keras.layers.Dense(250),
-            tf.keras.layers.Dropout(rate=0.01),
-            tf.keras.layers.Dense(128),
-            tf.keras.layers.Dense(4),
-        ]
+def train_model():
+    ((x_train, y_train), (x_test, y_test)) = load.load_training_data(
+        train_tables=10000,
+        test_tables=2000,
+        predict_time=1,
+        rows_per_table=100,
+        predict_param="high",
     )
+
+    model = tf.keras.Sequential([
+        tf.keras.layers.Dense(250, input_shape=(100, 5)),
+        tf.keras.layers.Dense(250),
+        tf.keras.layers.Dense(250),
+        tf.keras.layers.Dense(250),
+        tf.keras.layers.Dense(1)
+    ])
 
     optimizer = tf.keras.optimizers.RMSprop()
-    model_frame.compile(
-        optimizer=optimizer,
-        loss="mean_squared_error",
-        metrics=["mean_absolute_error", "mean_squared_error"],
-    )
+    model.compile(optimizer=optimizer,
+                  loss='mean_squared_error',
+                  metrics=['mean_absolute_error', 'mean_squared_error'])
 
-    return model_frame
-
-
-def train_model(training_model, epochs):
-    ((x_train, y_train), (x_test, y_test)) = load.load_training_data(
-        train_tables=150000, test_tables=20000, predict_time=1, rows_per_table=rows
-    )
-
-    training_model.fit(x_train, y_train, epochs=epochs)
-    training_model.evaluate(x_test, y_test)
-
-    return training_model
+    model.fit(x_train, y_train, epochs=5)
 
 
 if __name__ == "__main__":
-    # model = build_model()
-    model = tf.keras.models.load_model("scratch_model.md5")
-    model = train_model(model, 2)
-    model.save("scratch_model.md5")
+    train_model()
